57f14deb383c734aef5ab368dd10c1dbc992e563
vineeth <vineethsuper12345@gmail.com>
2021-12-11 14:35:34 +0530

    added structure files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import Home from "./components/home/Home.js";
 
 function App() {
-  return (
-    <Home />;
+  return <Home />;
 }
 
-export default App
+export default App;
